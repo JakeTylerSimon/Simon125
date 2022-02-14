04ed7631bec091442cf41210915f54e75656f2b2
Jake Simon <Jtsimonbaseball@gmail.com>
2022-02-14 01:43:31 -0800

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Home } from './component/Home/Home';
+import { Navbar } from './component/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
